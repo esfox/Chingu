4b4b63839be164658bc503db8324a354822e08c9
Glitch (esfox-chingu) <none>
2019-04-25 12:38:20 +0000

    Fixed teacher.js

--- modules/teacher.js
+++ modules/teacher.js
@@ -23,7 +23,7 @@ module.exports = class
             message.channel.send(message.response
                 .problem("You don't have permission to use this command."));
         }
-        
+            
         this.message = message;
     }
 
