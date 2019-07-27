12e80144a6c02d0e2372872cdd300a9608d1371c
Glitch (esfox-chingu) <none>
2019-07-27 03:15:28 +0000

    🤗👋 Checkpoint ./bot.js:1901482/1437 ./watch.json:1901482/486 ./modules/base.js:1901482/4275

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
-const http = require('http');  
+const http = require('http');
 const express = require('express');
 const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
+app.get("/", (request, response) =>
+{
+	console.log(Date.now() + " Ping Received");
+	response.sendStatus(200);
 });
 app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+setInterval(() =>
+{
+	http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
 const Discord = require('discord.js')
@@ -23,66 +25,65 @@ const config = require('./data/config.json');
 
 const args = process.argv.slice(2).shift();
 bot
-    .login(process.env.TOKEN || args)
-    .catch(error => { if(error) console.log('gitgud haha') });
+	.login(process.env.TOKEN || args)
+	.catch(error => { if(error) console.log('gitgud haha') });
 
 bot.on('ready', async _ =>
 {
-    await database.init();
-    console.log('Bot Started');
+	await database.init();
+	console.log('Bot Started');
 
-    reminder.startWordReminder(remindWordOfTheDay);
-    console.log(`Started Word of the Day reminder.\n(${new Date()})`);
+	reminder.startWordReminder(remindWordOfTheDay);
+	console.log(`Started Word of the Day reminder.\n(${new Date()})`);
 });
 
 commands.addModules(fs.readdirSync('./modules')
-    .filter(file =>
-        file.indexOf('.') !== 0 && 
-        file.slice(-3) === '.js')
-    .map(file => require(`./modules/${file}`)));
+	.filter(file =>
+		file.indexOf('.') !== 0 &&
+		file.slice(-3) === '.js')
+	.map(file => require(`./modules/${file}`)));
 
 bot.commands = commands;
 
 bot.on('message', message =>
 {
-    commands.handle(message);
+	commands.handle(message);
 
-    if(message.content === '?goodbot')
-        return message.channel.send('Me? 😏');
-}); 
+	if(message.content === '?goodbot')
+		return message.channel.send('Me? 😏');
+});
 
 function remindWordOfTheDay()
 {
-    const embed = new Discord.RichEmbed()
-        .setColor(config.embedColor)
-        .setTitle('🔔 Word of the Day Reminder');
+	const embed = new Discord.RichEmbed()
+		.setColor(config.embedColor)
+		.setTitle('🔔 Word of the Day Reminder');
 
-    const words = data.daily_words;
-    if(words.length === 0)
-    {
-        embed.setDescription("**There is no next word of the day!**");
-        return send();
-    }
+	const words = data.daily_words;
+	if(words.length === 0)
+	{
+		embed.setDescription("**There is no next word of the day!**");
+		return send();
+	}
 
-    const wordData = words.shift();
-    fs.writeFile(`./data/data.json`, JSON.stringify(data, null, '\t'), 
-        error =>
-        {
-            if(error) 
-                return console.error(error);
+	const wordData = words.shift();
+	fs.writeFile(`./data/data.json`, JSON.stringify(data, null, '\t'), error =>
+	{
+		if(error)
+			return console.error(error);
 
-            embed.addField('Next Word...', `${wordData.word}`
-                + ` = ${wordData.meaning}`);
-            embed.addField('Post Text', '```' + wordData.post + '```');
-            embed.setFooter(`${words.length} words saved.`);
-                
-            send();
-        });
+		embed.addField('Next Word...', `${wordData.word}`
+			+ ` = ${wordData.meaning}`);
+		embed.addField('Post Text', '```' + wordData.post + '```');
+		embed.setFooter(`${words.length} words saved.`);
 
-    function send()
-    {
-        bot.channels.get('385956869077860352')
-            .send(`<@&${config.teacher}>`, embed)
-            .catch(console.error);
-    }
+		send();
+	});
+
+	function send()
+	{
+		bot.channels.get('385956869077860352')
+			.send(`<@&${config.teacher}>`, embed)
+			.catch(console.error);
+	}
 }
--- modules/base.js
+++ modules/base.js
@@ -1,119 +1,119 @@
 const { prefix, owner } = require('../data/config.json');
 
 exports['ping, ㅍ'] = message => message.channel.send(message.response
-    .embed(`${~~message.client.ping} ms`));
+	.embed(`${~~message.client.ping} ms`));
 
 exports['help, ㄷ'] = message =>
 {
-    const description = `**Command Prefix: \`${prefix}\`**\n`
-        + 'I have **Korean** versions of my **commands**!\n‍';
+	const description = `**Command Prefix: \`${prefix}\`**\n`
+		+ 'I have **Korean** versions of my **commands**!\n‍';
 
-    const modules =
-    [
-        {
-            title: '🌐 Translation Commands',
-            content: 
-                '**`translate / ㅂ <text to translate>`** *(번역 - translation)*\n'
-                + ' ‍ ‍ ‍ ‍ ‍→ translates the text provided to Korean or English'
-                + '(automatic)\n'
-                + `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㅂ I'm learning Korean.\`\n\n`
+	const modules =
+		[
+			{
+				title: '🌐 Translation Commands',
+				content:
+					'**`translate / ㅂ <text to translate>`** *(번역 - translation)*\n'
+					+ ' ‍ ‍ ‍ ‍ ‍→ translates the text provided to Korean or English'
+					+ '(automatic)\n'
+					+ `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㅂ I'm learning Korean.\`\n\n`
 
-                + '**`dictionary / ㅅㅈ <word>`** *(사전 - dictionary)*\n'
-                + ' ‍ ‍ ‍ ‍ ‍→ searches the word in a dictionary (Naver).\n'
-                + `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㅅㅈ music\`\n‍`
-        },
-        {
-            title: '📒 Notebook Commands',
-            content:
-                '**`note / ㄴ <note>`** *(노트 - note)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ write a note on your notebook.\n'
-                + ` ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴ An important link`
-                    + ' https://papago.naver.com/?sk=ko&tk=en`\n\n'
+					+ '**`dictionary / ㅅㅈ <word>`** *(사전 - dictionary)*\n'
+					+ ' ‍ ‍ ‍ ‍ ‍→ searches the word in a dictionary (Naver).\n'
+					+ `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㅅㅈ music\`\n‍`
+			},
+			{
+				title: '📒 Notebook Commands',
+				content:
+					'**`note / ㄴ <note>`** *(노트 - note)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ write a note on your notebook.\n'
+					+ ` ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴ An important link`
+					+ ' https://papago.naver.com/?sk=ko&tk=en`\n\n'
 
-                + '**`notebook / ㄱㅊ`** *(공책 - notebook)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows your notebook.\n\n'
+					+ '**`notebook / ㄱㅊ`** *(공책 - notebook)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows your notebook.\n\n'
 
-                + '**`nfind / ㄴㅊ <text to find>`** *(찾다 - to find)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ find text in your notebook.\n'
-                + `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅊ hangul\`\n\n`
+					+ '**`nfind / ㄴㅊ <text to find>`** *(찾다 - to find)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ find text in your notebook.\n'
+					+ `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅊ hangul\`\n\n`
 
-                + '**`nedit / ㄴㅂ <note number> <new note>`**'
-                    + ' *(변하다 - to change)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ edit a note in your notebook by __note number__.\n'
-                + ` ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅂ 5 new note for note 5\`\n\n`
+					+ '**`nedit / ㄴㅂ <note number> <new note>`**'
+					+ ' *(변하다 - to change)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ edit a note in your notebook by __note number__.\n'
+					+ ` ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅂ 5 new note for note 5\`\n\n`
 
-                + '**`ndelete / ㄴㅅ <note number>`** *(삭제 - deletion)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ delete a note in your notebook by __note number__.\n'
-                + `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅅ 1\`\n‍`
-        },
-        {
-            title: '📝 Exercise Commands (has very few content atm)',
-            content: 
-                '**`kp / ㅋ`** *(keyboard practice/키보드)*\n'
-                + ' ‍ ‍ ‍ ‍ ‍→ shows a random Hangul character and asks you to type'
-                + ' it as practice to memorize the Korean keyboard (in PC).\n\n'
+					+ '**`ndelete / ㄴㅅ <note number>`** *(삭제 - deletion)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ delete a note in your notebook by __note number__.\n'
+					+ `‍ ‍ ‍ ‍ ‍ ‍Example: \`${prefix}ㄴㅅ 1\`\n‍`
+			},
+			{
+				title: '📝 Exercise Commands (has very few content atm)',
+				content:
+					'**`kp / ㅋ`** *(keyboard practice/키보드)*\n'
+					+ ' ‍ ‍ ‍ ‍ ‍→ shows a random Hangul character and asks you to type'
+					+ ' it as practice to memorize the Korean keyboard (in PC).\n\n'
 
-                + '**`kpw / ㅋㄷ`** *(keyboard practice - word/키보드 단어)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a random word in Hangul and asks you to type it'
-                    + ' also as practice to memorize the Korean keyboard'
-                    + ' (in PC).\n\n'
+					+ '**`kpw / ㅋㄷ`** *(keyboard practice - word/키보드 단어)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a random word in Hangul and asks you to type it'
+					+ ' also as practice to memorize the Korean keyboard'
+					+ ' (in PC).\n\n'
 
-                + '**`noun / ㅁㅅ`** *(명사 - noun)*\n'  
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a noun and asks you to translate it.\n\n'
+					+ '**`noun / ㅁㅅ`** *(명사 - noun)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a noun and asks you to translate it.\n\n'
 
-                + '**`verb / ㄷㅅ`** *(동사 - verb)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a verb and asks you to translate it.\n\n'
+					+ '**`verb / ㄷㅅ`** *(동사 - verb)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a verb and asks you to translate it.\n\n'
 
-                + '**`phrase / ㄱㅈ`** *(구절 - Phrase)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a phrase asks you to translate it.\n‍\n'
+					+ '**`phrase / ㄱㅈ`** *(구절 - Phrase)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a phrase asks you to translate it.\n‍\n'
 
-                + '**`nnum / ㅎㅅ`** *(한국 수 - Korean Number)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a native korean number and asks you to'
-                    + ' translate it.\n\n'
+					+ '**`nnum / ㅎㅅ`** *(한국 수 - Korean Number)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a native korean number and asks you to'
+					+ ' translate it.\n\n'
 
-                + '**`snum / ㅈㅅ`** *(중국 수 - Chinese Number)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a sino-korean number and asks you to'
-                    + ' translate it.\n\n'
+					+ '**`snum / ㅈㅅ`** *(중국 수 - Chinese Number)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a sino-korean number and asks you to'
+					+ ' translate it.\n\n'
 
-                + '**`conjugate / ㅎㅇ`** *(활용 - Conjugation)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows a verb then asks you to conjugate it to a'
-                    + ' random conjugation.\n‍\n'
-        },
-        {
-            title: 'ℹ Other Commands',
-            content: 
-                '**`ping / ㅍ`**\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows the ping of the bot.\n\n'
+					+ '**`conjugate / ㅎㅇ`** *(활용 - Conjugation)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows a verb then asks you to conjugate it to a'
+					+ ' random conjugation.\n‍\n'
+			},
+			{
+				title: 'ℹ Other Commands',
+				content:
+					'**`ping / ㅍ`**\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows the ping of the bot.\n\n'
 
-                + '**`help / ㄷ`** *(돕다 - to help)*\n'
-                + '‍ ‍ ‍ ‍ ‍ ‍→ shows this message.'
-        }
-    ]
+					+ '**`help / ㄷ`** *(돕다 - to help)*\n'
+					+ '‍ ‍ ‍ ‍ ‍ ‍→ shows this message.'
+			}
+		]
 
-    const response = message.response.embed('안녕하세요!')
-        .setDescription(description)
-        .setThumbnail(message.client.user.displayAvatarURL)
-        .setFooter('Ping @esfox#2053 for questions/feedback.');
+	const response = message.response.embed('안녕하세요!')
+		.setDescription(description)
+		.setThumbnail(message.client.user.displayAvatarURL)
+		.setFooter('Ping @esfox#2053 for questions/feedback.');
 
-    modules.forEach(m => response.addField(m.title, m.content));
-    message.channel.send(response);
+	modules.forEach(m => response.addField(m.title, m.content));
+	message.channel.send(response);
 }
 
 const schedule = require('node-schedule');
 
 exports.나연 = message =>
 {
-    if(message.author.id !== owner)
-        return;
+	if(message.author.id !== owner)
+		return;
 
-    message.channel.send(`Started Timers. ${new Date()}`);
+	message.channel.send(`Started Timers. ${new Date()}`);
 
-    let date = Date.now();
-    date += 10000;
+	let date = Date.now();
+	date += 10000;
 
-    const task = schedule.scheduleJob(date, _ =>
-    {
-        message.channel.send(`Task Trigger\n${new Date()}`);
-        task.cancel();
-    });
+	const task = schedule.scheduleJob(date, _ =>
+	{
+		message.channel.send(`Task Trigger\n${new Date()}`);
+		task.cancel();
+	});
 }
