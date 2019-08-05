e4beaaefd93914babd13b5f45ddc6ab68a0b31ad
Glitch (esfox-chingu) <none>
2019-08-05 02:59:13 +0000

    🏤🔔 Checkpoint ./server/wesite.js:1901482/326 ./bot.js:1901482/402

--- bot.js
+++ bot.js
@@ -1,16 +1,4 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) =>
-{
-	console.log(Date.now() + " Ping Received");
-	response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() =>
-{
-	http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+require('./server/website');
 
 const Discord = require('discord.js')
 const bot = new Discord.Client();
