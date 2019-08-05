7c0c55e9b88968b696b5714fb7826dd934ad0a47
Glitch (esfox-chingu) <none>
2019-08-05 12:00:36 +0000

    🕵💈 Checkpoint ./server/website.js:1901482/64 ./server/website/css/desktop.ini:1901482/132 ./server/website/css/style.css:1901482/844 ./server/website/css/materialize.css:1901482/179196 ./server/website/css/materialize copy.css:1901482/179196 ./server/website/css/materialize.min.css:1901482/425487

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-app.get("/", (request, response) =>
+app.get("/", (_, response) =>
 {
 	console.log(Date.now() + " Ping Received");
 	response.sendStatus(200);
 });
+
+app.use(express.static(''))
+
 app.listen(process.env.PORT);
+
 setInterval(() =>
 {
 	http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+}, 280000);	
