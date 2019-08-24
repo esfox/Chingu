e1ce84b8f7d9b33fd875b715a791b789ffde633e
Glitch (esfox-chingu) <none>
2019-08-24 10:35:41 +0000

    🏏🏍 Checkpoint ./package.json:1901482/914 ./server/index.js:1901482/69

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const wordOfTheDay = require('../utils/word-of-the-day');
 
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('server/website'));
 
